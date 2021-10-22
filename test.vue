53d33444fa549dc51b490fc450a3c35bd62d8764
evan <1366197200@qq.com>
2021-10-22 11:11:05 +0800

    feat: 增加webgl模板

---
+++ test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <li></li>
+        <li></li>
+        <li>
+            <img/>
+        </li>
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style module>
+.foo {
+    background: red;
+}
+</style>
